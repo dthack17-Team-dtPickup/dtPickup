5194456b3a5ec9d0b224f9535ae6cb5a7bf8b2bf
roflrox <rolf.kimmelmann@gmx.de>
2017-09-19 21:08:25 +0200

    mock profile

--- src/services/ProfileService/profile.service.ts
+++ src/services/ProfileService/profile.service.ts
@@ -3,19 +3,38 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Profile } from './profile';
 
+
 @Injectable()
 export class ProfileService {
 
+    dbRef: any;
+    hits = new BehaviorSubject([])
+  
+    constructor(private db: AngularFireDatabase) {
+      /// Reference database location for GeoFire
+      this.dbRef = this.db.list('/profiles');
+     
+     }
+
+    PROFILES:Profile[] = [
+        {id: '0001', name: 'Rüdiger', rating: 4, car:{ id: '0001', make:'Opel', model:'Astra', colour: 'blue', seats:5} }
+    ];
 
 
 
  getProfile(id: string){
+return {id: '0001', name: 'Rüdiger', rating: 4, car:{ id: '0001', make:'Opel', model:'Astra', colour: 'blue', seats:5} };
 
  }
 
 
 addProfile(profile:Profile){
-    
+ return true;
+}
+
+getProfiles(){
+    return this.PROFILES;
 }
 
 }
+
