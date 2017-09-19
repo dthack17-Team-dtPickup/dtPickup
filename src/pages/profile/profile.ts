6ad5815da9d3f74900f332635fdfa54386340166
Thomas Cujé <tcuje@freenet.de>
2017-09-19 22:04:30 +0200

    update profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProfileService } from '../../services/ProfileService/profile.service';
 import { AuthService } from '../../services/AuthService/auth.service';
 //import { TemplateService } from '../../services/TemplateService/template.service';
 
+@Pipe({
+  name: 'fill'
+})
+export class FillPipe implements PipeTransform {
+  transform(value) {
+    return (new Array(value)).fill(1);
+  }
+}
+
 @Component({
   selector: 'page-profile',
-  templateUrl: 'profile.html'
+  templateUrl: 'profile.html',
+  pipes: [FillPipe]
 })
 
 export class ProfilePage {
-  
+
   user_profile: any;
   templates: any;
-  
+
   constructor(public navCtrl: NavController, auth: AuthService, profileService: ProfileService) {
     this.user_profile = profileService.getProfile(auth.currentUserId);
 //    this.templates = templateService.getTemplates(auth.currentUserId);
