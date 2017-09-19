f5867022b7d5e1cca6a05e7ca73c0d436667a431
roflrox <rolf.kimmelmann@gmx.de>
2017-09-19 20:09:31 +0200

    init ProfileService

---
+++ src/services/ProfileService/profile.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable()
+export class ProfileService {
+
+    
+}
